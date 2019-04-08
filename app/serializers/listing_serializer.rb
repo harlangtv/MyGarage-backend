e1056c1b21f66cd171e92091c733e63c87e34553
github username <github email address>
2019-04-08 12:58:55 -0400

    fixed listing serializer

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,5 +1,6 @@
 class ListingSerializer < ActiveModel::Serializer
 
   attributes :id, :user_id, :vehicle_make, :vehicle_model, :vehicle_year, :mileage, :vehicle_zip_code, :transmission, :vehicle_description
+  has_many :images
 
 end
